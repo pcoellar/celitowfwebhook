10bb993f1ab42136928ac14763d38f5ce4f309b2
pcoellar <pedrocoellarburbano@hotmail.com>
2024-07-16 07:21:18 -0500

    initial structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommonModule } from './common/common.module';
+import { WfwebhookModule } from './wfwebhook/wfwebhook.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CommonModule, WfwebhookModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3030);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.setGlobalPrefix('api/v1');
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Celito WF WebHook')
+    .setDescription('Celito WF WebHook Manager')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(3031);
 }
 bootstrap();
